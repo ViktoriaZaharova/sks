029f0fe9e0349f4e4431468aea4d21c6918a6ae3
Viktoria <zaharova181114@gmail.com>
2022-02-12 23:22:10 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,14 @@
+//плавный скролл
+$(document).ready(function () {
+    $('.go_to').click(function (e) {
+        e.preventDefault();
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length !== 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 500);
+        }
+        return false;
+    });
+});
+//плавный скролл end
