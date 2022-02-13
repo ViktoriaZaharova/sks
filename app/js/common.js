552ce0a725e119e21b53b1abdaccc8b0d3567941
Viktoria <zaharova181114@gmail.com>
2022-02-14 00:10:10 +0200

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -12,3 +12,59 @@ $(document).ready(function () {
     });
 });
 //плавный скролл end
+
+$('.btn-add-reviews').on('click', function (e) {
+    e.preventDefault();
+    $('.leave-feedback').slideDown();
+    $('.btn-group-wrap').fadeOut();
+});
+
+$('.btn-hidden-reviews').on('click', function (e) {
+    e.preventDefault();
+    $('.leave-feedback').slideUp();
+    $('.btn-group-wrap').fadeIn();
+});
+
+// slick
+$('.certificates-slider').slick({
+    slidesToShow: 6,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon fill-none"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon fill-none"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 1400,
+            settings: {
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+$('.btn-tooltip').hover(function (e) {
+    e.preventDefault();
+    $(this).find('.tooltip-box').fadeToggle();
+});
+
+$('.btn-burger').on('click', function () {
+   $('.fixed-block').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.fixed-block').fadeOut();
+});
